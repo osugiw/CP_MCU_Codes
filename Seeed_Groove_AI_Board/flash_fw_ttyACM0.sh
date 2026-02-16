7225844b75941192d558e79fba5f4b10da988262
Sugiarto Wibowo <osugiartow@gmail.com>
2026-02-16 10:59:14 -0600

    SEEED groove settings

---
+++ Seeed_Groove_AI_Board/flash_fw_ttyACM0.sh
@@ -0,0 +1,8 @@
+# Flash to the device new firmware
+echo  "Flashing firmware to Groove AI Vision 2 Board on ttyACM0....."
+echo  "sudo setfacl -m u:[USERNAME]:rw /dev/ttyUSB0"
+python3 xmodem/xmodem_send.py --port=/dev/ttyACM0 --baudrate=921600 --protocol=xmodem --file=we2_image_gen_local/output_case1_sec_wlcsp/output.img
+
+
+# Launch minicom
+minicom -s
