cbadcb492715c0a2c697e81e564af8da453106e3
Sugiarto Wibowo <osugiartow@gmail.com>
2026-04-03 12:12:53 -0500

    update repo folder

--- Seeed_Groove_AI_Board/flash_factory.sh
+++
@@ -1,6 +0,0 @@
-echo  "Flashing firmware to Groove AI Vision 2 Board on ttyACM0....."
-sudo setfacl -m u:osugi_w:rw /dev/ttyACM0
-python3 xmodem/xmodem_send.py --port=/dev/ttyACM0 --baudrate=921600 --protocol=xmodem --file=Seeed_SenseCraft_AI_2023_12_19T183401_402.img
-
-# example:
-# python3 xmodem/xmodem_send.py --port=/dev/ttyACM0 --baudrate=921600 --protocol=xmodem --file=we2_image_gen_local/output_case1_sec_wlcsp/output.img
